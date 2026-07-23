9ab3e661ed3cdb05d9066508951065031c63c7ea
anikaj-eng <anikaj@nvidia.com>
2026-07-23 13:48:13 -0700

    Add tunable prefetching to DeviceSelect::Flagged
    Expose compile-time prefetch levels and add focused correctness and performance coverage.

--- cub/cub/device/dispatch/dispatch_select_if.cuh
+++ cub/cub/device/dispatch/dispatch_select_if.cuh
@@ -223,7 +223,8 @@ struct make_vsmem_helper
                            active_policy.scan_algorithm,
                            delay_constructor_t<active_policy.lookback_delay.kind,
                                                active_policy.lookback_delay.delay,
-                                               active_policy.lookback_delay.l2_write_latency>>;
+                                               active_policy.lookback_delay.l2_write_latency>,
+                           active_policy.load_prefetch>;
   using type = vsmem_helper_default_fallback_policy_t<
     agent_policy_t,
     bind_selection_opt<SelectionOpt>::template agent_t,
--- cub/test/catch2_test_device_select_env.cu
+++ cub/test/catch2_test_device_select_env.cu
@@ -1337,7 +1337,8 @@ C2H_TEST("Test SelectPolicy properties", "[select][device]")
     cub::BlockLoadAlgorithm::BLOCK_LOAD_DIRECT,
     cub::CacheLoadModifier::LOAD_DEFAULT,
     cub::BlockScanAlgorithm::BLOCK_SCAN_WARP_SCANS,
-    cub::LookbackDelayPolicy{cub::LookbackDelayAlgorithm::fixed_delay, 350, 450}};
+    cub::LookbackDelayPolicy{cub::LookbackDelayAlgorithm::fixed_delay, 350, 450},
+    cub::detail::LoadPrefetch::l2};
 
 #  if _CCCL_STD_VER >= 2020
   // designated init
@@ -1348,7 +1349,8 @@ C2H_TEST("Test SelectPolicy properties", "[select][device]")
     .load_modifier     = cub::CacheLoadModifier::LOAD_DEFAULT,
     .scan_algorithm    = cub::BlockScanAlgorithm::BLOCK_SCAN_WARP_SCANS,
     .lookback_delay    = cub::LookbackDelayPolicy{
-         .kind = cub::LookbackDelayAlgorithm::fixed_delay, .delay = 350, .l2_write_latency = 450}};
+         .kind = cub::LookbackDelayAlgorithm::fixed_delay, .delay = 350, .l2_write_latency = 450},
+    .load_prefetch = cub::detail::LoadPrefetch::l2};
 #  else // _CCCL_STD_VER >= 2020
   constexpr auto p2 = p1;
 #  endif // _CCCL_STD_VER >= 2020
@@ -1367,6 +1369,7 @@ C2H_TEST("Test SelectPolicy properties", "[select][device]")
              ", .load_algorithm = BLOCK_LOAD_DIRECT, .load_modifier = LOAD_DEFAULT"
              ", .scan_algorithm = BLOCK_SCAN_WARP_SCANS"
              ", .lookback_delay = LookbackDelayPolicy { .kind = LookbackDelayAlgorithm::fixed_delay"
-             ", .delay = 350, .l2_write_latency = 450 } }");
+             ", .delay = 350, .l2_write_latency = 450 }"
+             ", .load_prefetch = detail::LoadPrefetch::l2 }");
 }
 #endif // _CCCL_COMPILER(GCC, >=, 8)
